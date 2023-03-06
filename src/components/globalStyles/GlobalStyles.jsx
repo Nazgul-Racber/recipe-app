36be1b4779d91dc9618748a7c7280d24a985f4d1
email <racbernazgul@gmail.com>
2023-03-07 01:36:25 +0300

    globalStyles

--- src/components/globalStyles/GlobalStyles.jsx
+++ src/components/globalStyles/GlobalStyles.jsx
@@ -6,6 +6,7 @@ export default GlobalStyles = createGlobalStyle`
     margin:0;
     padding:0;
     box-sizing: border-box;
+    font-family: "Grassol",cursive;
 }
 
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import GlobalStyles from "../components/globalStyles/GlobalStyles";
 import Navbar from './../components/nav/Navbar';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <GlobalStyles/>
       <Navbar/>
       <Routes>
         <Route/>
